225edd0395f6efee95b0d7f69488ef4b6a005424
Amirul Islam <amirulandalib9@gmail.com>
2022-07-26 23:51:55 +0600

    FUCKCK

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ git clone https://github.com/meganz/sdk.git --depth=1 -b v$MEGA_SDK_VERSION ~/ho
 && autoupdate -fIv && ./autogen.sh \
 && ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples --with-python3 \
 && make -j$(nproc --all) \
+&& cd bindings/python/ && python3 setup.py bdist_wheel \
+&& cd dist && pwd
