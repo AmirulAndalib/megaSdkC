77e6d76a0bf4dde57fe5c16c32eb779a47b52478
Amirul Islam <amirulandalib9@gmail.com>
2022-07-26 23:59:25 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export MEGA_SDK_VERSION="3.12.3"
 git clone https://github.com/meganz/sdk.git --depth=1 -b v$MEGA_SDK_VERSION ~/home/sdk \
 && cd ~/home/sdk && rm -rf .git \
 && autoupdate -fIv && ./autogen.sh \
-&& ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples --with-python3 \
+&& ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples  \
 && make -j$(nproc --all) \
 && cd bindings/python/ && python3 setup.py bdist_wheel \
 && cd dist && pwd
