0d25fd127f4dac0b048adec28ee6bec85d9b0125
Amirul Islam <amirulandalib9@gmail.com>
2022-07-28 22:41:04 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 export PYTHONWARNINGS=ignore
-export MEGA_SDK_VERSION="4.0.2"
-git clone https://github.com/meganz/sdk.git --depth=1 -b v$MEGA_SDK_VERSION ~/home/sdk \
+git clone https://github.com/meganz/sdk.git --depth=1 -b v$master ~/home/sdk \
 && cd ~/home/sdk && rm -rf .git \
 && autoupdate -fIv && ./autogen.sh \
 && ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples  \
