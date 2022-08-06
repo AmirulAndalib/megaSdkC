87555725660fa81610914ef9e1c82421e60ba71a
AmirulAndalib <amirulandalib9@gmail.com>
2022-08-06 20:09:26 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 export PYTHONWARNINGS=ignore
-git clone https://github.com/meganz/sdk.git --depth=1 -b v$master ~/home/sdk \
+git clone https://github.com/meganz/sdk.git -b master ~/home/sdk \
 && cd ~/home/sdk && rm -rf .git \
 && autoupdate -fIv && ./autogen.sh \
-&& ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples  \
+&& ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples --with-python3 \
 && make -j$(nproc --all) \
 && cd bindings/python/ && python3 setup.py bdist_wheel \
 && cd dist && pwd
