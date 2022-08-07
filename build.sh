c008ab7408c3c7f950506e4cd527d112a2c2c450
Amirul Islam <amirulandalib9@gmail.com>
2022-08-07 07:01:40 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@ git clone https://github.com/meganz/sdk.git -b master ~/home/sdk \
 && cd ~/home/sdk && rm -rf .git \
 && autoupdate -fIv && ./autogen.sh \
 && ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples --with-python3 \
-&& make -j$(nproc --all) \
-&& cd bindings/python/ && python3 setup.py bdist_wheel \
-&& cd dist && pwd
+&& make -j$(nproc --all)
